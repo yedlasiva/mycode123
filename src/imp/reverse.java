5832604886b48b47be577173568819fa42f35907
unknown <yedlasiva95@gmail.com>
2024-06-02 23:15:07 +0530

    my code change

---
+++ src/imp/reverse.java
@@ -0,0 +1,26 @@
+package imp;
+
+public class reverse {
+
+	public static String getreverseofstring(String s)
+	{
+		String rev = "";
+
+		//siva
+		for(int i = s.length()-1;i>=0;i--)
+		{
+			char ch =s.charAt(i);
+			System.out.println(ch);
+			rev = rev+ch;
+		}
+		System.out.println("rev="+rev);
+		return rev;
+
+	}
+
+	public static void main(String[] args) {
+	
+		getreverseofstring("siva");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module interviewimportquestions {
+}
